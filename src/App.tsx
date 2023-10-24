a26ea1d2aaa74c719eaf5bcfe3f06bf13687eb09
Avi Avinav <aviavinav2004@gmail.com>
2023-10-24 23:36:58 +0530

    make basic ui & add jotai package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Todo from "./todo";
 
 function App() {
-	return <div></div>;
+	const handleSubmit = () => { };
+	return (
+		<div className="container">
+			<h1 className="headerText">My Jotai Todo App</h1>
+			<p className="description">
+				This is a simple todo app made using Jotai and React (and obviously
+				Typescript). That is pretty much it I guess, go ahead and try it.
+			</p>
+			<div className="addTodo">
+				<input type="text" />
+				<button onSubmit={handleSubmit}>+ Add Todo</button>
+			</div>
+			<div className="todoList">
+				<Todo />
+			</div>
+		</div>
+	);
 }
 
 export default App;
